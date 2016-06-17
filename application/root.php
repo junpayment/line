1260ee13906da38a163a031d1a1d371fd3447923
nomura <nomura-junpei@dmm.com>
2016-06-17 13:43:13 +0900

    unlink a not necessary library.

--- application/root.php
+++ application/root.php
@@ -1,7 +1,6 @@
 <?php
 defined('APP_PATH') || define('APP_PATH', dirname(__FILE__));
 
-require_once(APP_PATH . '/ext/line-bot-sdk-php/src/LINEBot.php');
 require_once(APP_PATH . '/Logic.php');
 require_once(APP_PATH . '/Log.php');
 require_once(APP_PATH . '/Auth.php');
